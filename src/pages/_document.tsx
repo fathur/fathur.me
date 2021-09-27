3321a41823235812bea783c8dfb480fce90603ce
Fathur Rohman <hi.fathur.rohman@gmail.com>
2021-09-27 18:06:38 +0700

    add gtag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,8 +15,28 @@ class MyDocument extends Document {
   render() {
     return (
       <Html className="bg-gray-100">
-        <Head />
+        <Head>
+          {/* eslint-disable-next-line @next/next/next-script-for-ga */}
+          <script
+            id="gtag"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-WQVKHVX');`,
+            }}
+          />
+        </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WQVKHVX"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
