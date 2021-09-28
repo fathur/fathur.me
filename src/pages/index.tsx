17a6f6e0d7c0bc2a7e98c02ce1b6831b9a39a7c6
Fathur Rohman <hi.fathur.rohman@gmail.com>
2021-09-28 11:00:51 +0700

    add additional meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,9 +23,13 @@ const Home: NextPage = () => {
       <Head>
         <title>Fathur</title>
 
-        {/*<script dangerouslySetInnerHTML={{ __html: nightwind.init() }} />*/}
+        <meta
+          name="viewport"
+          content="width = device-width, initial-scale = 1.0, minimum-scale = 1, maximum-scale = 1, user-scalable = no"
+        />
 
         <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-title" content="Fathur" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
