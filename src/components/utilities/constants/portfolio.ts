f2933c9951a3c22847876ec40096fc4f695be611
Fathur Rohman <fathur.rohman@julofinance.com>
2025-09-15 07:20:08 +0700

    add real portfolio

--- src/components/utilities/constants/portfolio.ts
+++ src/components/utilities/constants/portfolio.ts
@@ -1,9 +1,10 @@
 export const PortfolioData = [
-    {
-        title: 'Agung Wibowo',
-        description: 'Professional portfolio website developed for Agung Wibowo, a personal branding specialist at Southeast Asia',
-        image: '/images/portfolio/agungwibowo.png',
-        link: 'https://agungwibowo.com/',
-        technologies: ['Wordpress', 'HTML']
-    }
-]
+  {
+    title: 'Agung Wibowo',
+    description:
+      'Professional portfolio website developed for Agung Wibowo, a personal branding specialist at Southeast Asia',
+    image: '/images/portfolio/agungwibowo.png',
+    link: 'https://agungwibowo.com/',
+    technologies: ['Wordpress', 'HTML'],
+  },
+];
